c4be9f65fe58c2dadbec32e581656c9d57c6e020
ElchinMammadov <ekodrive@gmail.com>
2026-03-06 13:23:08 +0400

    Enable Bootstrap 5 pagination in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Models\Setting;
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\ServiceProvider;
 
@@ -15,6 +16,7 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        Paginator::useBootstrapFive();
         $this->configureMailFromDb();
     }
 
